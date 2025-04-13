3b413b2152d121746764ce5b0e769968742e987e
Andrey Khokhlov <progeat@mail.ru>
2025-04-14 00:56:23 +0300

    feat: add initialize of the project logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
+import { buttons } from './buttons';
+import { Sound } from './sounds';
 import './index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttonsContainer = document.querySelector('.buttons-container');
+  const volumeControl = document.getElementById('volume');
+
+  console.log(buttons);
+
+  const summerSound = new Sound('summer');
+  const rainSound = new Sound('rain');
+  const winterSound = new Sound('winter');
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const soundName = button.dataset.sound;
+
+      switch (soundName) {
+        case 'summer':
+          summerSound.play();
+          break;
+        case 'rain':
+          rainSound.play();
+          break;
+        case 'winter':
+          winterSound.play();
+          break;
+      }
+    });
+  });
+
+  buttonsContainer.append(...buttons);
+
+  volumeControl.addEventListener('input', ({ target }) => {
+    const volume = target.value / 100;
+    document.querySelectorAll('audio').forEach((audio) => {
+      audio.volume = volume;
+    });
+  });
+});
