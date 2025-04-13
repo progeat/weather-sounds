176ee702409ec78657f5fa352f40b2aa050e6d4c
Andrey Khokhlov <progeat@mail.ru>
2025-04-14 00:22:58 +0300

    fix: change the attribute of the buttons

--- src/buttons.js
+++ src/buttons.js
@@ -2,8 +2,8 @@ import DATA from './data';
 
 const buttons = DATA.map((elementData) => {
   const button = document.createElement('button');
-  button.setAttribute.dataId = elementData.id;
-  button.classList.add('button', elementData.sound);
+  button.setAttribute.dataSound = elementData.sound;
+  button.classList.add('button');
   button.style.backgroundImage = `url('./assets/icons/${elementData.icon}.svg')`;
 
   return button;
