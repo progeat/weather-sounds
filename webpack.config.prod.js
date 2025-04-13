ef8a8cfa38ad333f0c1aeaf20c6746b431ca2a21
Andrey Khokhlov <progeat@mail.ru>
2025-04-13 17:26:26 +0300

    build: install the terser plugin
    Для возврата минификации файлов js в prod сборку

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,11 +1,13 @@
 const { merge } = require('webpack-merge');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const commonConfig = require('./webpack.config.common');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
   devtool: 'source-map',
   optimization: {
-    minimizer: [new CssMinimizerPlugin()],
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
   },
 });
