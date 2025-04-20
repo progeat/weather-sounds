45807dc5cb8094d4721ba0129156685d0c9b8ea5
Andrey Khokhlov <progeat@mail.ru>
2025-04-20 21:03:53 +0300

    build: add typescript support

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -11,6 +11,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
@@ -54,6 +57,11 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
